8e9de64267227d8ed37b419647b3d888bf90e6fa
Bruno <carlitos078955@gmail.com>
2023-07-27 09:58:30 -0500

    Agregando validacion de campos

--- validacion.js
+++ validacion.js
@@ -1,1 +1,56 @@
-//Haz tú validación en javascript acá
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]) {
+      validadores[tipoDeInput](input);
+    }
+
+    if(input.validity.valid){
+        input.parentElement.classList.remove("input-container--invalid")
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    }else{
+        input.parentElement.classList.add("input-container--invalid")
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input);
+    }
+}
+
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError"
+];
+const mensajesDeError = {
+    //objeto, tipos de input
+    nombre: {
+        valueMissing: "El campo nombre no puede estar vacio"
+    },
+    email: {
+
+        valueMissing: "El campo correo no puede estar vacio",
+        typeMismatch: "El correo no es valido"
+    },
+    asunto: {
+        valueMissing: "El campo asunto no puede estar vacio",
+    },
+
+};
+
+const validadores = {
+   // nacimiento: (input) => validarNacimiento(input),
+};
+
+
+function mostrarMensajeDeError(tipoDeInput,input){
+    let mensaje = "";
+    tipoDeErrores.forEach( error => {
+        if(input.validity[error]){
+            console.log(tipoDeInput, error);
+            console.log(input.validity[error]);
+            console.log(mensajesDeError[tipoDeInput][error])
+            mensaje = mensajesDeError[tipoDeInput][error]
+        }
+    })
+    
+    return mensaje;
+}
+
